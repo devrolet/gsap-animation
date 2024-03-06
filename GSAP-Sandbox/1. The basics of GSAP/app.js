0332a5cbb3a0f157ddbcb37e7c3bdbde2549d5dc
Chet Hill <chetdhill@gmail.com>
2024-03-06 15:50:37 -0600

    Utility methods for tweens

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,5 +1,23 @@
 console.log("GSAP ENGINE: ", gsap);
 const img1 = document.querySelector(".img1");
 
-gsap.set(img1, {opacity: 0});
-gsap.set(".img2", {backgroundColor: "green"});
+
+// TODO: Tween methods: https://gsap.com/docs/v3/GSAP/Tween
+const tween = gsap.to('.img3', {y: 200});
+
+// Delays the tween (seconds)
+tween.delay(1);
+
+tween.duration(3);
+
+
+setTimeout(() => {
+    // Resume your tween
+    // tween.resume();
+
+    tween.seek(2.5)
+    tween.play()
+}, 2000);
+
+// Kills the tween
+// tween.kill();
