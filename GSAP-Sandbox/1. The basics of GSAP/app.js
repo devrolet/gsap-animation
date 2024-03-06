9b44d84bd7282c73c8dbc41261eb98e2d241af99
Chet Hill <chetdhill@gmail.com>
2024-03-06 12:13:33 -0600

    GSAP.TO method

---
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -0,0 +1,7 @@
+console.log(gsap);
+
+// GSAP.TO METHOD ARGS:(targetelement,  animationObj: {x, y, duration(0.5 default), })
+gsap.to('.img1', {x: 100, y: 100, backgroundColor: "red", duration: 2, rotate: 45});
+
+// You can also use `img${1}` to loop
+
