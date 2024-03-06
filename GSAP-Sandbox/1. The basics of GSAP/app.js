1cb0a9c1037d3a4942cc549daade24027efa784f
Chet Hill <chetdhill@gmail.com>
2024-03-06 13:05:46 -0600

    GSAP Easing

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,10 +1,6 @@
 console.log('GSAP ENGINE: ', gsap);
 const img1 = document.querySelector('.img1');
 
-
-// gsap.from(img1, {autoAlpha: 0, rotation: 45, scale: 0.5, x: 100, y: 100, duration: 2});
-
-document.querySelector('.btn').addEventListener('click', () => {
-gsap.from(img1, {autoAlpha: 0}, {autoAlpha: 1, duration: 2});
-});
+// EASING IN GSAP: See more at https://gsap.com/docs/v3/Eases
+gsap.from(img1, {autoAlpha: 0, y: -100, rotation: 90, ease: 'bounce', duration: 2});
 
