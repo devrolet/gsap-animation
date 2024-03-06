e9d0a3e77fbaf6fc802b0bfe31b7d5826b158d2a
Chet Hill <chetdhill@gmail.com>
2024-03-06 16:07:15 -0600

    GSAP: Position parameters

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,11 +1,12 @@
 console.log("GSAP ENGINE: ", gsap);
 
-// TIMELINES IN GSAP
+// TIMELINES IN GSAP (Greensock Animation Platform)
 const TL = gsap.timeline();
 
+// Timeline: Position parameters ("+/-=number", "< or >", 3 (absolute)). Set by seconds
 TL
-.from('.img1', {autoAlpha: 0, y: -50, duration: 2})
-.from('.img2', {autoAlpha: 0, y: -50, duration: 2})
-.from('.img3', {autoAlpha: 0, y: -50, duration: 2})
-.from('h1', {autoAlpha: 0, y: 50, duration: 2})
-.from('p', {autoAlpha: 0, y: 50, duration: 2});
+.from('.img1', {autoAlpha: 0, y: -50, duration: 1})
+.from('.img2', {autoAlpha: 0, y: -50, duration: 1}, '-=0.75') // add third arg if you want TL to trigger something early(-) or later(+).
+.from('.img3', {autoAlpha: 0, y: -50, duration: 1}, '<')
+.from('h1', {autoAlpha: 0, y: 50, duration: 1}, '-=0.75')
+.from('p', {autoAlpha: 0, y: 50, duration: 1}, '-=0.75');
