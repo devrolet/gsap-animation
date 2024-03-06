ecbb52594c12fe262e1424e31504f20cd7ecc49a
Chet Hill <chetdhill@gmail.com>
2024-03-06 13:13:04 -0600

    GSAP Utility Props

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,6 +1,16 @@
-console.log('GSAP ENGINE: ', gsap);
-const img1 = document.querySelector('.img1');
+console.log("GSAP ENGINE: ", gsap);
+const img1 = document.querySelector(".img1");
 
 // EASING IN GSAP: See more at https://gsap.com/docs/v3/Eases
-gsap.from(img1, {autoAlpha: 0, y: -100, rotation: 90, ease: 'bounce', duration: 2});
-
+// Utility Props: More found in the GSAP docs
+gsap.from(img1, {
+  autoAlpha: 0,
+  y: -100,
+  rotation: 90,
+  ease: "bounce",
+  duration: 2,
+  delay: 0.5,
+  repeat: 2,
+  repeatDelay: 0.2,
+  yoyo: true
+});
