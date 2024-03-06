df9ee04a42494083a0501a0106b338bb5cdcaffb
Chet Hill <chetdhill@gmail.com>
2024-03-06 15:29:36 -0600

    GSAP: Trigger event callback functions

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,16 +1,12 @@
 console.log("GSAP ENGINE: ", gsap);
 const img1 = document.querySelector(".img1");
 
-// Animate random values, Stagger group of buttons
-// gsap.to(".btn", { 
-//     duration: 0.5, 
-//     stagger: 0.2, 
-//     x: "random(-100, 100)" // adding another # like 10 will round the random number by 10
-//     // you can also add an array. for example x: "random([20, 50, 60, -30, -70])"
-// });
-
-gsap.to(img1, {keyframes: [
-    {duration: 0.3, x: 100},
-    {duration: 0.3, y: 100},
-    {duration: 0.3, x: 200},
-]});
+gsap.to(img1, { 
+    x: 100, 
+    duration: 1, 
+    repeat: 1, 
+    onComplete: () => console.log("COMPLETE"),
+    onStart: () => console.log("START"),
+    onUpdate: () => console.log("UPDATE"),
+    onRepeat: () => console.log("REPEAT")
+});
