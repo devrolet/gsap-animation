b64ca02bc75287164b516f0c715a865211a8cd9d
Chet Hill <chetdhill@gmail.com>
2024-03-06 16:13:38 -0600

    Properties and methods of the timeline

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,12 +1,22 @@
 console.log("GSAP ENGINE: ", gsap);
 
 // TIMELINES IN GSAP (Greensock Animation Platform)
-const TL = gsap.timeline();
+const TL = gsap.timeline({
+    defaults: {
+        duration: 1,
+        ease: "power4"
+    },
+    // repeat: -1,
+    // yoyo: true,
+    onComplete: () => console.log("COMPLETE"),
+    onStart: () => console.log("START")
+
+});
 
 // Timeline: Position parameters ("+/-=number", "< or >", 3 (absolute)). Set by seconds
 TL
-.from('.img1', {autoAlpha: 0, y: -50, duration: 1})
-.from('.img2', {autoAlpha: 0, y: -50, duration: 1}, '-=0.75') // add third arg if you want TL to trigger something early(-) or later(+).
-.from('.img3', {autoAlpha: 0, y: -50, duration: 1}, '<')
-.from('h1', {autoAlpha: 0, y: 50, duration: 1}, '-=0.75')
-.from('p', {autoAlpha: 0, y: 50, duration: 1}, '-=0.75');
+.from('.img1', {autoAlpha: 0, y: -50})
+.from('.img2', {autoAlpha: 0, y: -50}) // add third arg if you want TL to trigger something early(-) or later(+).
+.from('.img3', {autoAlpha: 0, y: -50})
+.from('h1', {autoAlpha: 0, y: 50})
+.from('p', {autoAlpha: 0, y: 50});
