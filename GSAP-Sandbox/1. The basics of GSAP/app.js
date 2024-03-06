3dac379a6bd13ba91f98be77d6325c2938ec233f
Chet Hill <chetdhill@gmail.com>
2024-03-06 16:18:46 -0600

    Add and AddLabel methods

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -19,4 +19,10 @@ TL
 .from('.img2', {autoAlpha: 0, y: -50}) // add third arg if you want TL to trigger something early(-) or later(+).
 .from('.img3', {autoAlpha: 0, y: -50})
 .from('h1', {autoAlpha: 0, y: 50})
+.addLabel('endAnim')
 .from('p', {autoAlpha: 0, y: 50});
+
+setTimeout(() => {
+    TL.seek('endAnim')
+    TL.play()
+}, 1000)
