13ac13c19c94d8215e025bc8d219ea09a6fc8222
Chet Hill <chetdhill@gmail.com>
2024-03-06 15:39:04 -0600

    Setting values in GSAP

--- GSAP-Sandbox/1. The basics of GSAP/app.js
+++ GSAP-Sandbox/1. The basics of GSAP/app.js
@@ -1,17 +1,5 @@
 console.log("GSAP ENGINE: ", gsap);
 const img1 = document.querySelector(".img1");
 
-gsap.registerEffect({
-  name: "imgAnimation",
-  effect: (targets, config) => {
-    return gsap.to(targets, {
-      duration: config.duration,
-      y: 200,
-      scale: 1.4,
-      rotation: 360,
-    });
-  },
-  defaults: { duration: 2 },
-});
-
-gsap.effects.imgAnimation("img", {duration: 5})
+gsap.set(img1, {opacity: 0});
+gsap.set(".img2", {backgroundColor: "green"});
